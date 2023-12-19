d2bef06185b3614f8e8bd0ca45e10b7fc0c48ac6
aounhe <>
2023-12-19 14:27:25 +0000

    I created this variable to create a list where I can query all the data from the films database
    I have added Review table as well because it will appear all the users reviews in
         each film once submited

--- filmmanager/routes.py
+++ filmmanager/routes.py
@@ -6,3 +6,13 @@ from filmmanager.models import Film, Review, Users
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/films")
+def films():
+   
+   films = list(film.query.order_by(Film.id_film, Film.title, Film.director, Film.year,
+                                     Film.genre, Film.image, Film.description).all())
+   reviews = list(Review.query.order_by(Review.film_id, Review.review_text,
+                                         Review.users_review).all())
+    return render_template("filmss.html", films=films, reviews=reviews)
