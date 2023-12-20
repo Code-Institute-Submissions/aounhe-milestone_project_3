d88a312a446ede92f09e4b0b9bbbb11ca22478ec
aounhe <>
2023-12-20 16:01:58 +0000

    Creat functions to generate templates

--- filmmanager/routes.py
+++ filmmanager/routes.py
@@ -1,18 +1,18 @@
 from flask import render_template
 from filmmanager import app, db
-from filmmanager.models import Film, Review, Users
+from filmmanager.models import film
 
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-
 @app.route("/films")
 def films():
-   
-   films = list(film.query.order_by(Film.id_film, Film.title, Film.director, Film.year,
-                                     Film.genre, Film.image, Film.description).all())
-   reviews = list(Review.query.order_by(Review.film_id, Review.review_text,
-                                         Review.users_review).all())
-    return render_template("filmss.html", films=films, reviews=reviews)
+    return render_template("films.html")
+
+
+@app.route("/add_review", methods=["GET", "POST"])
+def add_review():
+    return render_template("add_review.html")
+
