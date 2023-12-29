bf66d02c9f288ff7ba3e86edea1db52bebcffa2d
aounhe <>
2023-12-29 17:34:21 +0000

    tidy up

--- filmmanager/models.py
+++ filmmanager/models.py
@@ -1,26 +1,6 @@
 from filmmanager import db, app
 
 
-class Film(db.Model):
-
-    # schema for films model
-    id_film = db.Column(db.String, unique=True, primary_key=True)
-    # film title unique
-    title = db.Column(db.String(50), unique=True, nullable=False)
-    director = db.Column(db.String, nullable=False)  # film director
-    year = db.Column(db.Date, nullable=False)  # film year
-    genre = db.Column(db.String, nullable=False)  # film genre
-    overview = db.Column(db.String, nullable=False)  # film description
-    film_reviews = db.relationship(
-        "Review", backref="film", cascade="all, delete", lazy=True)
-
-
-def __repr__(self):
-    # __rep__ to represent itself in the form of a string
-    return "#{0} - genre: {1} | Users: {4}".format
-    (self.id_film, title, director, year, genre, overview)
-
-
 class Review(db.Model):
     # schema for Reviews model
     id = db.Column(db.Integer, primary_key=True)
@@ -33,7 +13,7 @@ class Review(db.Model):
     def __repr__(self):
         # to represent id in the form of a string
         return "#{0} - id: {1} | Users: {4}".format(
-        self.id, self.film_id, self.users_review
+            self.id, self.film_id, self.users_review
         )
 
 
@@ -53,3 +33,26 @@ class Users(db.Model):
         return "# Email: {0} - Name: {2} - Last Name: {3}".format(
             self.id_email, self.fname, self.lname
         )
+
+
+class Film(db.Model):
+
+    # schema for films model
+    id_film = db.Column(db.String, unique=True, primary_key=True)
+    # film title unique
+    title = db.Column(db.String(50), unique=True, nullable=False)
+    director = db.Column(db.String, nullable=False)  # film director
+    year = db.Column(db.Date, nullable=False)  # film year
+    genre = db.Column(db.String, nullable=False)  # film genre
+    overview = db.Column(db.String, nullable=False)  # film description
+    film_reviews = db.relationship(
+        "Review", backref="film", cascade="all, delete", lazy=True)
+    image = db.Column(db.String)
+
+def __repr__(self):
+    # __rep__ to represent itself in the form of a string
+    return "#{0} - title: {1} | image: {2}".format
+    (self.id_film, title, director, year, genre, overview, film_reviews)
+
+
+
