acfb5b3e1a45415a2fa5f24521e5dd3a837a4410
aounhe <>
2024-01-03 21:56:18 +0000

    Import session from flask
    Add method get and post to film app
    
    Add review list
    
    Add login to the users account from the database, if the user request Post, they will need to add email and password from the database

--- filmmanager/routes.py
+++ filmmanager/routes.py
@@ -1,6 +1,6 @@
-from flask import render_template, request, redirect, url_for, flash
+from flask import render_template, request, redirect, url_for, flash, session
 from filmmanager import app, db
-from filmmanager.models import Film, Review, Users
+from filmmanager.models import Film, Users, Review
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
@@ -9,13 +9,16 @@ def home():
     return render_template("index.html")
 
 
-@app.route("/films", methods=["GET", "POST"])
+@app.route("/films")
 def films():
+
     films = list(Film.query.order_by(Film.id_film, Film.title, Film.director, Film.year,
                                      Film.genre, Film.image, Film.overview).all())
-    return render_template("films.html", films=films)
-
-
+    reviews = list(Review.query.order_by(Review.film_id, Review.review_text,
+                                         Review.users_review).all())
+    
+    return render_template("films.html", films=films, reviews=reviews)
+    
 
 
 @app.route("/signup", methods=["GET", "POST"])
@@ -28,11 +31,9 @@ def signup():
         fname = request.form.get("fname")
         lname = request.form.get("lname")
 
-        # Hash the password before storing it
         # Hash the password before storing it to the database so the password is save.
         password_hash = generate_password_hash(password)
 
-        # Check if the user with the given email already exists
         # Check if the user with the given email already exists,
         #  if it exists it will print a message
         existing_user = Users.query.filter_by(id_email=id_email).first()
@@ -40,6 +41,7 @@ def signup():
         if existing_user:
             flash('Email already in use. Please choose a different email.', 'error')
             return redirect(url_for("signup"))
+
         # Create a new Users instance with hashed password
         user = Users(
             id_email=id_email,
@@ -47,43 +49,73 @@ def signup():
             fname=fname,
             lname=lname
         )
+
         # Add the user to the database
         db.session.add(user)
         db.session.commit()
-
         # Once the account it is create it will get redirected to this site.
         return redirect(url_for("profile"))
 
     return render_template("signup.html")
 
 
-@app.route("/signin", methods=["GET"])
+@app.route("/signin", methods=["GET", "POST"])
 def signin():
-    """
-    if request.method == "GET":
-        users = Users(
-            id_email = request.form.get("id_email"),
-            password = request.form.get("password"),
-        )
-        db.session.add(users)
-        db.session.commit()
-        return redirect(url_for("profile"))
-    """
+    """Login to the users account from the database, if the user request Post, they will need
+    to add email and password from the database"""
+
+    if request.method == "POST":
+        email = request.form.get("id_email")
+        password = request.form.get("password")
+
+        user = Users.query.filter_by(id_email=email).first()
+        # if the user does not exists it will print this message
+        if not user:
+            flash('Invalid email address. Please try again.', 'error')
+            return redirect(url_for("signin"))
+        # if the password is incorrect then it will get this message
+        if not check_password_hash(user.password, password):
+            flash('Invalid password. Please try again.', 'error')
+            return redirect(url_for("signin"))
+
+        # Successful login is equal to True, user_id = user.id_email
+
+        session['logged_in'] = True
+        session['user_id'] = user.id_email
+        # once log in it will get to the profile site.
+        return redirect(url_for('profile'))
+
     return render_template("signin.html")
+
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
 
 
 @app.route("/profile", methods=["GET", "POST"])
 def profile():
+    """ the user will only see the profile once it is login, if it is not login then profile
+    will be hidden"""
+    # Check if the user is logged in
+    if not session.get('logged_in'):
+        # Redirect to the login page if not logged in
+        return redirect(url_for('signin'))
 
-    return render_template("profile.html")
+        # Fetch the user data from the database
+    user_id = session.get('user_id')
+    user = Users.query.filter_by(id_email=user_id).first()
+
+    return render_template("profile.html", user=user)
 
 
 @app.route("/add_review", methods=["GET", "POST"])
 def add_review():
-    """When the user clicks 'Add Review' button in the profile page, this will use the'GET'
-       method and render the 'add_review' template. Once they submit the form, this
-       will call the same function, but will check if the request being made is a “POST“ method,
-       which posts data somewhere, such as a database. """
+
+    films = list(Film.query.order_by(Film.id_film, Film.title, Film.director, Film.year,
+                                     Film.genre, Film.image, Film.overview).all())
     if request.method == "POST":
         # add a review to the database.
         # if I use request method I have to import it to flask as well on the top.
@@ -96,5 +128,5 @@ def add_review():
         db.session.commit()
         # after the form gets submitted, and I have added and committed
         # the new data to our database, I can redirect the user to the 'profile' page.
-        return redirect(url_for("films"))
-    return render_template("add_review.html")
+        return redirect(url_for("add_review"))
+    return render_template("add_review.html", films=films)
